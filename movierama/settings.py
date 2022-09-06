690e2407f3df3fed4afba944e513d9b7bd7b0c38
pzerzis <pzerzis@wings-ict-solutions.eu>
2022-09-06 14:37:35 +0300

    add movie model and gitignore

--- movierama/settings.py
+++ movierama/settings.py
@@ -9,12 +9,11 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-1fq^!)4h^vn^2akri1*5i(@w6g6+ncofdrvyr#w$b#--p*n-2-
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -70,17 +68,18 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'movierama.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'movierama',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': os.getenv('POSTGRES_HOST', '127.0.0.1'),
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -100,7 +99,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -111,7 +109,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
--- movierama_app/models.py
+++ movierama_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Movie(models.Model):
+    title = models.TextField()
+    description = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_movies')
+    timestamp = models.DateTimeField()
+    n_likes = models.PositiveIntegerField()
+    n_hates = models.PositiveIntegerField()
+
+    def __str__(self):
+        return 'title:{}, user:{}, timestamp:{}'.format(self.name, self.user.username, self.timestamp)
+
+    class Meta:
+        ordering = ['timestamp']
